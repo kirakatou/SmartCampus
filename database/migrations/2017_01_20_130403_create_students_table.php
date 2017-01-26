46145897181a1f2992fe676d76635cae11022d0d
Sutandi Tan <Sutandi Tan>
2017-01-27 01:49:48 +0700

    add student table migration file

---
+++ database/migrations/2017_01_20_130403_create_students_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim')->unique();
+            $table->string('name');
+            $table->integer('gender');
+            $table->date('birthdate');
+            $table->string('religion');
+            $table->integer('department_id')->unsigned();
+            $table->foreign('department_id')
+                  ->references('id')->on('departments')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->string('classname');
+            $table->string('image')->nullable();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
