6631a2c499e5e39657aa997d2b7a316c8059e29e
Sutandi Tan <Sutandi Tan>
2017-01-27 01:50:20 +0700

    add staff table migration file

---
+++ database/migrations/2017_01_20_132110_create_staffs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staffs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sid');
+            $table->string('name');
+            $table->integer('gender');
+            $table->date('birthdate');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staffs');
+    }
+}
