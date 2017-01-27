42c32225ac0a082aac5d1bbe5bef2d41741ef061
Sutandi Tan <Sutandi Tan>
2017-01-28 01:45:21 +0700

    declare datetime on dates var

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    protected $fillable = [
+    	'name', 'datetime', 'location', 'type', 'pay', 'price', 'capacity', 'image', 'banner', 
+    	'poster', 'description'
+    ];
+
+    protected $dates = ['datetime'];
+
+    public function setPayAttribute($value)
+    {
+    	$this->attributes['pay'] = (boolean)($value);
+    }
 }
