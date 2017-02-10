de27020362c83a338cebab3f5109a15e6825ebbc
Sutandi Tan <Sutandi Tan>
2017-02-10 20:45:08 +0700

    add routing for admin page

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,42 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', 'IndexController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+
+	Route::get('/join/event/{id?}', 'IndexController@store');
+	Route::get('/cancel/event/{id}', 'IndexController@update');
+	
 });
+
+Route::get('/alert', function() {
+    \Alert::message('Hey!');
+
+    return view('errors.503');
+});
+
+Route::resource('/department', 'DepartmentController');
+
+Route::resource('/student', 'StudentController');
+
+Route::resource('/event', 'EventController');
+
+Route::resource('/staff', 'StaffController');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::group(['middleware' => 'App\Http\Middleware\AdminMiddleware'], function(){
+	    Route::get('/admin', function(){
+	        return 'testt';
+	    });
+
+	});
+});
+// Route::get('images/{folder}/{filename}', function ($folder, $filename)
+// {
+//     return Image::make(storage_path() . '/' . $folder . '/' . $filename)->response();
+// });
