57fd44d7a855ddcdfb1a2afc7abab0d9a810218e
Sutandi Tan <Sutandi Tan>
2017-02-19 01:19:43 +0700

    make PaymentMail to send mailable for invoice

--- app/Mail/PaymentMail.php
+++ app/Mail/PaymentMail.php
@@ -10,15 +10,19 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 class PaymentMail extends Mailable
 {
     use Queueable, SerializesModels;
-
+    public $no, $name, $event_name, $price, $valid_date;
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($no, $name, $event_name, $price, $valid_date)
     {
-        //
+        $this->no = $no;
+        $this->name = $name;
+        $this->event_name = $event_name;
+        $this->price = $price;
+        $this->valid_date = $valid_date;
     }
 
     /**
@@ -28,6 +32,8 @@ class PaymentMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Payment Invoice for ' . $this->event_name . ' Event')
+                    ->from('smartcampus@hmjsi.org')
+                    ->view('emails.payment');
     }
 }
